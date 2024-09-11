273286f4f83951a5c744c107d960fdd45cc98cae
Jacob Veerapen <jacobveerapen@gmail.com>
2024-09-11 20:17:17 +0100

    special cases

--- processing/scraper.py
+++ processing/scraper.py
@@ -7,6 +7,54 @@ class AthleticsDataScraper:
         self.base_url = 'https://www.alltime-athletics.com/'
         self.gender = gender
 
+    def generate_url(self, event, is_legal):
+        special_cases_male = {
+            '100m': ('m_100ok.htm', 'm100mno.htm'),
+            'trip': ('mtripok.htm', 'mtripno.htm'),
+            'long': ('mlongok.htm', 'mlongno.htm'),
+            '110h': ('m_110hok.htm', 'm_110hno.htm'),
+            'pole': ('mpoleok.htm', 'mpoleno.htm'),
+            'shot': ('mshotok.htm', 'mshotno.htm'),
+            'disc': ('mdiscok.htm', 'mdiscno.htm'),
+            'jave': ('mjaveok.htm', 'mjaveno.htm'),
+            'hamm': ('mhammok.htm', 'mhammno.htm'),
+            'deca': ('mdecaok.htm', 'mdecano.htm'),
+            '60m': ('m60mok.htm', 'm60mno.htm'),
+            '800m': ('m_800ok.htm', 'm_800no.htm'),
+            '1500m': ('m_1500ok.htm', 'm_1500no.htm'),
+            '5000m': ('m_5000ok.htm', 'm_5000no.htm'),
+            '10000': ('m_10kok.htm', '10kno.htm')
+        }
+
+        special_cases_female = {
+            '100m': ('w_100ok.htm', 'w_100no.htm'),
+            'trip': ('wtripleok.htm', 'wtripleno.htm'),
+            'long': ('wlongok.htm', 'wlongno.htm'),
+            '100h': ('w_100hok.htm', 'w_100hno.htm'),
+            'pole': ('wpoleok.htm', 'wpoleno.htm'),
+            'shot': ('wshotok.htm', 'wshotno.htm'),
+            'disc': ('wdiscok.htm', 'wdiscno.htm'),
+            'jave': ('wjaveok.htm', 'wjaveno.htm'),
+            'hamm': ('whammok.htm', 'whammno.htm'),
+            'hept': ('whepaok.htm', 'whepano.htm'),
+            '60m': ('w60mok.htm', 'w60mno.htm'),
+            '800m': ('w_800ok.htm', 'w_800no.htm'),
+            '1500m': ('w_1500ok.htm', 'w_1500no.htm'),
+            '5000m': ('w_5000ok.htm', 'w_5000no.htm'),
+            '10000': ('w_10kok.htm', 'w_10kno.htm')
+        }
+
+        special_cases = special_cases_male if self.gender == 'male' else special_cases_female
+
+        if event in special_cases:
+            legal_suffix, illegal_suffix = special_cases[event]
+            suffix = legal_suffix if is_legal else illegal_suffix
+        else:
+            suffix = f"{self.gender[0]}_{event}{'ok' if is_legal else 'no'}.htm"
+
+        return f"{self.base_url}{suffix}"
+
+
     def fetch_data(self, event, is_legal):
         url = self.generate_url(event, is_legal)
         response = requests.get(url)
